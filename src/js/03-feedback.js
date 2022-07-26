e09e783ea8f6b988b24a3d84c3a8f0cdb3170880
AnastasiyaSeroshtan <97408274+AnastasiyaSeroshtan@users.noreply.github.com>
2022-07-26 18:06:00 +0300

    adds a second solution to 3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -33,3 +33,40 @@ function handleSubmitButtonClick(event) {
 }
 
 
+// ===============2 variant===================
+
+// import throttle from 'lodash.throttle';
+// const formEl = document.querySelector(".feedback-form");
+// const inputEl = document.querySelector(".feedback-form input");
+// const textareaEl = document.querySelector(".feedback-form textarea");
+// const LOCAL_KEY = "feedback-form-state";
+
+// formEl.addEventListener('input', throttle(onInputForm, 500));
+// formEl.addEventListener('submit', onSubmitButtonClick);
+
+// function onInputForm(e) {
+//     const mail = formEl.elements.email.value;
+//     const message = formEl.elements.message.value;
+
+//     const formData = {
+//         email:mail,
+//         message:message,
+//     };
+
+//     localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
+// }
+
+// const formValue = localStorage.getItem(LOCAL_KEY);
+
+// if (formValue) {
+//     const formDataParse = JSON.parse(formValue);
+//     inputEl.value = formDataParse.email || "";
+//     textareaEl.value = formDataParse.message || "";
+// }
+
+// function onSubmitButtonClick (e) {
+//     e.preventDefault();
+//     console.log(JSON.parse(localStorage.getItem(LOCAL_KEY)));
+//     localStorage.removeItem(LOCAL_KEY);
+//     e.currentTarget.reset();
+// }
